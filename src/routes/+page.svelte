9e75ea572ae9d6118b240a9fb12844f18a9e21bc
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-05-21 16:00:04 +0100

    Mockup of the questions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
-  
+<div class="h-screen flex justify-center my-auto">
+    <div class="w-[30rem] bg-base-300 p-5 rounded-xl my-auto mx-4">
+        <h1 class="text-4xl mb-10 whitespace-pre-wrap">Select the correct characters for “やまのてせん”</h1>
+        <div class="grid grid-cols-2 grid-rows-2">
+            <button class="btn text-xl mr-1 mb-1 font-normal">山手線</button>
+            <button class="btn text-xl ml-1 mb-1 font-normal">点</button>
+            <button class="btn text-xl mr-1 mt-1 font-normal">原宿</button>
+            <button class="btn text-xl ml-1 mt-1 font-normal">交差点</button>
+        </div>
+    </div>
+</div>
