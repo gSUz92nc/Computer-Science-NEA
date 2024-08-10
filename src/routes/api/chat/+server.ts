4e878edaa270ae990b8c7c35c95626b5f801f033
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-08-10 21:16:13 +0000

    Formatted API code and updated packages

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,25 +1,25 @@
 // src/routes/api/chat/+server.ts
-import type { RequestHandler } from './$types'
-import { streamText } from 'ai'
-import { createAnthropic } from '@ai-sdk/anthropic'
-import { env } from '$env/dynamic/private'
+import type { RequestHandler } from "./$types";
+import { streamText } from "ai";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { env } from "$env/dynamic/private";
 
-const anthropic = createAnthropic({ apiKey: env.ANTHROPIC_API_KEY ?? ''})
+const anthropic = createAnthropic({ apiKey: env.ANTHROPIC_API_KEY ?? "" });
 
 export const POST = (async ({ request: req }) => {
   // Extract the `messages` from the body of the request
   const { messages } = await req.json();
 
   // Get a language model
-  const model = anthropic('claude-3-5-sonnet-20240620')
+  const model = anthropic("claude-3-haiku-20240307"); //'claude-3-5-sonnet-20240620')
 
   // Call the language model with the prompt
   const result = await streamText({
     model,
     messages,
     maxTokens: 1024,
-  })
+  });
 
   // Respond with a streaming response
-  return result.toAIStreamResponse()
-}) satisfies RequestHandler
+  return result.toAIStreamResponse();
+}) satisfies RequestHandler;
