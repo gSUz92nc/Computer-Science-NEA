ea118a7a60e02c392dff9a5dcd539ca39cf2a42d
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-09-19 18:01:31 +0100

    Added some files for the JLPT vocab as well as rotated supabase keys

---
+++ helper/jlpt/words/upload.ts
@@ -0,0 +1,10 @@
+// Import the Supabase client
+import type { Database } from "../../../src/lib/database.types";
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY || "";
+
+// Initialize the Supabase client
+const supabaseUrl = "https://yrykfurvxgrvavxgbvbr.supabase.co";
+const supabaseKey = SUPABASE_SERVICE_KEY;
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
