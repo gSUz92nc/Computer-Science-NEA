9912364efd37519249dd286c5c320f8d43acac15
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-09-20 19:23:06 +0000

    search changes

--- helper/search/test.ts
+++ helper/search/test.ts
@@ -9,13 +9,16 @@ const supabaseUrl = "https://yrykfurvxgrvavxgbvbr.supabase.co";
 const supabaseKey = SUPABASE_SERVICE_KEY;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
-let index = 0;
-let search: string = "あ";
+let search: string = "'example'";
 
 async function loadData() {
   const startTime = Date.now();
-  const { data, error } = await supabase
-    .rpc("search_jmdict", { search_term: search })
+  // const { data, error } = await supabase
+  //   .rpc("search_jmdict", { search_term: search })
+
+
+  const { data, error } = await supabase.from("jmdict_word").select("jmdict_sense ( id ( fts )), jmdict_kana ( * ), jmdict_kanji ( * )").textSearch("jmdict_sense.id.fts", search).eq("jmdict_kana.text", search).eq("jmdict_kanji.text", search);
+
 
   if (error) {
     console.error("Error fetching data:", error);
@@ -28,31 +31,29 @@ async function loadData() {
     return { success: false, timeTaken: endTime - startTime };
   }
 
-  index += 1;
-  console.log(index);
-
   return { success: true, timeTaken: endTime - startTime };
 }
 
 async function benchmarkLoadData() {
-  const numberOfRequests = 2000;
+  const numberOfRequests = 100;
   console.time("loadData");
 
-  const promises = Array.from({ length: numberOfRequests }, () => loadData());
-  const results = await Promise.allSettled(promises);
+  let totalTime = 0;
+  let failures = 0;
+  let success = 0;
 
-  const totalTime = results.reduce((acc, result) => {
-    if (result.status === "fulfilled") {
-      return acc + result.value.timeTaken;
+  for (let i = 0; i < numberOfRequests; i++) {
+    const result = await loadData();
+    if (result.success) {
+      totalTime += result.timeTaken;
+      success += 1;
+      console.log(success, "/", numberOfRequests);
+    } else {
+      failures += 1;
     }
-    return acc;
-  }, 0);
+  }
 
   const averageTimePerRequest = totalTime / numberOfRequests;
-  const failures =
-    results.filter((result) =>
-      result.status === "rejected" || !result.value.success
-    ).length;
 
   console.timeEnd("loadData");
   console.log(`Number of failures: ${failures}`);
