9867facae6fcc151c84cd54ad59d1f5bc95bf699
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-09-23 14:30:01 +0100

    trying to fix  search

--- helper/search/test.ts
+++ helper/search/test.ts
@@ -2,27 +2,46 @@
 import type { Database } from "../../src/lib/database.types";
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY || "";
+const SUPABASE_SERVICE_KEY = process.env.PUBLIC_SUPABASE_ANON_KEY || "";
 
 // Initialize the Supabase client
 const supabaseUrl = "https://yrykfurvxgrvavxgbvbr.supabase.co";
 const supabaseKey = SUPABASE_SERVICE_KEY;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
-let search: string = "'example'";
+let search: string = "example";
 
 async function loadData() {
   const startTime = Date.now();
   // const { data, error } = await supabase
   //   .rpc("search_jmdict", { search_term: search })
 
-
-  const { data, error } = await supabase.from("jmdict_word").select("jmdict_sense ( id ( fts )), jmdict_kana ( * ), jmdict_kanji ( * )").textSearch("jmdict_sense.id.fts", search).eq("jmdict_kana.text", search).eq("jmdict_kanji.text", search);
-
+  const { data, error } = await supabase
+    .from("jmdict_word")
+    .select(
+      `
+          id,
+          jmdict_sense ( *, jmdict_gloss ( id, text ) )
+      `,
+    )
+    .eq("jmdict_sense.jmdict_gloss.id", "2e8bf32d-2b58-40d3-9684-809dde9e4b83") // Assuming this is the id of jmdict_sense
+    .limit(100);
 
   if (error) {
     console.error("Error fetching data:", error);
   }
+
+  console.log(data ? console.log(data) : console.log("No data"));
+
+  // Save to json file
+  const fs = require("fs");
+  fs.writeFile("data.json", JSON.stringify(data), (err: any) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("File has been created");
+  });
 
   const endTime = Date.now();
 
@@ -31,11 +50,12 @@ async function loadData() {
     return { success: false, timeTaken: endTime - startTime };
   }
 
+  console.log(data.length);
   return { success: true, timeTaken: endTime - startTime };
 }
 
 async function benchmarkLoadData() {
-  const numberOfRequests = 100;
+  const numberOfRequests = 1;
   console.time("loadData");
 
   let totalTime = 0;
