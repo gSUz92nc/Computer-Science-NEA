63ef08d78c491c7f53dec318111f7b70a6e4a5ef
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-09-26 16:50:22 +0100

    still working on the dictionary

--- helper/search/test.ts
+++ helper/search/test.ts
@@ -9,33 +9,66 @@ const supabaseUrl = "https://yrykfurvxgrvavxgbvbr.supabase.co";
 const supabaseKey = SUPABASE_SERVICE_KEY;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
-let search: string = "'example'";
+let search: string = "'hello'";
 
 async function loadData() {
   const startTime = Date.now();
+
   // const { data, error } = await supabase
-  //   .rpc("search_jmdict", { search_term: search })
+  //   .from("jmdict_gloss")
+  //   .select("*, jmdict_sense( word_id )") // This selects all columns from jmdict_gloss and all columns from jmdict_sense
+  //   .textSearch("fts", search);
 
+  // if (error) {
+  //   console.error("Error fetching data:", error);
+  // }
 
-  const { data, error } = await supabase.from("jmdict_word").select("jmdict_sense ( id ( fts )), jmdict_kana ( * ), jmdict_kanji ( * )").textSearch("jmdict_sense.id.fts", search).eq("jmdict_kana.text", search).eq("jmdict_kanji.text", search);
+  const wordId = 1000040; // Replace with the actual word ID
 
-
-  if (error) {
-    console.error("Error fetching data:", error);
-  }
-
+  // Check if the word exists
+  // Fetch all related data if the word exists
+  const { data: testData, error: testError } = await supabase
+    .from("jmdict_word")
+    .select(`
+      *,
+      jmdict_sense (
+        *,
+        jmdict_gloss (
+          *
+        )
+      ),
+      jmdict_kana (*),
+      jmdict_kanji (*)
+    `)
+    .textSearch("jmdict_sense.jmdict_gloss.fts", search).limit(1000);
+    
   const endTime = Date.now();
 
-  if (error) {
-    console.error("Error loading data:", error);
-    return { success: false, timeTaken: endTime - startTime };
+  if (testError) {
+    console.error("Error fetching word data:", testError);
+  } else {
+    console.log("Fetched word data:", testData);
+    console.log("Number of words fetched:", testData?.length);
+
+    // log how many unique glosses were fetched
+    const glosses = new Set<Object>();
+    testData?.forEach((word) => {
+      word.jmdict_sense.forEach((sense) => {
+        sense.jmdict_gloss.forEach((gloss) => {
+          glosses.add(gloss);
+        });
+      });
+    });
+
+    console.log("Number of unique glosses fetched:", glosses.size);
+    console.log(glosses);
   }
 
   return { success: true, timeTaken: endTime - startTime };
 }
 
 async function benchmarkLoadData() {
-  const numberOfRequests = 100;
+  const numberOfRequests = 1;
   console.time("loadData");
 
   let totalTime = 0;
