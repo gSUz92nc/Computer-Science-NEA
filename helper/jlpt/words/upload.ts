57f714cb820c0498e05e1e38789a3dc6187f15e5
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-10-07 15:32:46 +0100

    Got most of the upload functionality working just making it so that it works for each JLPT level

--- helper/jlpt/words/upload.ts
+++ helper/jlpt/words/upload.ts
@@ -19,30 +19,74 @@ const csv = Bun.file('n5.csv').text()
 // save the first value of each row to an array, excluding the first row as it is a header
 const kanjiArray = parse(await csv).slice(1)
 
-let responsesToWrite = []
+let emptyResults = []
+let kanjiCount = 0
 
 // search the jmdict_kanji table for the first value of eaach row then return create a new 2d array which contains the first value of each row and the result of the search whether the returned data is empty or not
-async function processKanjiArray() {
+async function processKanjiArray(csvItems: unknown[][] | Record<string, unknown>[], jlpt_level: number) {
   let index = 0
-  let emptyResults = []
 
   const kanjiArrayWithSearchResults = await Promise.all(
-    kanjiArray.map(async (kanji) => {
+    csvItems.map(async (kanji) => {
+      
+      if (kanji[0].includes('～')) {
+        kanji[0] = kanji[0].replace(/～/g, '');
+      }
+
+      const kanjiElements = kanji[0].split('; ');
+      kanjiCount += kanjiElements.length
+      
+      // Search for each kanji element seperately and check if there is one common word and only one common word
+      let commonWordId = null;
+
+      for (const element of kanjiElements) {
+        const { data: kanjiData } = await supabase
+          .from('jmdict_kanji')
+          .select('word_id')
+          .eq('text', element);
+
+        const { data: kanaData } = await supabase
+          .from('jmdict_kana')
+          .select('word_id')
+          .eq('text', element);
+
+        const combinedData = [...(kanjiData || []), ...(kanaData || [])];
+
+        const wordIds = combinedData.map((item) => item.word_id);
+        const uniqueWordIds = [...new Set(wordIds)];
+
+        if (uniqueWordIds.length === 1) {
+          commonWordId = uniqueWordIds[0];
+          break;
+        }
+      }
+
+      if (commonWordId) {
+        return [kanji[0], commonWordId];
+      }
+      
       console.log(index)
       index++
 
       const { data } = await supabase
         .from('jmdict_kanji')
-        .select('*, jmdict_word ( jmdict_sense ( jmdict_gloss ( * ) ) )')
+        .select('*, jmdict_word ( id, jmdict_sense ( jmdict_gloss ( * ) ) )')
         .eq('text', kanji[0])
-        .eq('common', true)
 
       if (data == null) return
 
       if (data?.length == 1) {
-        return
+        return [kanji[0], data[0].word_id]
       }
 
+      // If there are mutliple results check if there is one and only one with the common flag set to true
+      
+      const commonData = data.filter((x) => x.common)
+      
+      if (commonData.length == 1) {
+        return [kanji[0], commonData[0].word_id]
+      }
+      
       let highestText = ''
 
       // If there are mutliple kanji with the same text, find the one with the highest similarity
@@ -65,26 +109,51 @@ async function processKanjiArray() {
 
         console.log(kanji[0], highestText)
 
-        responsesToWrite.push(kanji[0], highestText)
-
-        return [kanji[0], data[highestSimilarityIndex]]
+        return [kanji[0], data[highestSimilarityIndex].word_id]
       }
 
       // If the data is empty, search for similar kanji
 
       const { data: kanaData } = await supabase
         .from('jmdict_kana')
-        .select('*')
+        .select('*, jmdict_word ( id, jmdict_sense ( jmdict_gloss ( * ) ) )')
         .eq('text', kanji[0])
-        .eq('common', true)
 
       if (kanaData?.length == 1) {
-        return
+        return [kanji[0], kanaData[0].word_id]
       }
+      
+      const commonKanaData = kanaData?.filter((x) => x.common)
+      
+      if (commonKanaData?.length == 1) {
+        return [kanji[0], commonKanaData[0].word_id]
+      }
+      
+      if (kanaData?.length > 1) {
+        let highestSimilarity = 0
+        let highestSimilarityIndex = 0
+        let highestText = ''
+        for (let i = 0; i < kanaData.length; i++) {
+          const senses = kanaData[i].jmdict_word?.jmdict_sense || []
+          for (const sense of senses) {
+            for (const gloss of sense.jmdict_gloss) {
+              const similarity = stringSimilarity(kanji[2], gloss.text)
+              if (similarity > highestSimilarity) {
+                highestText = gloss.text
+                highestSimilarity = similarity
+                highestSimilarityIndex = i
+              }
+            }
+          }
+        }
+
+        console.log(kanji[0], highestText)
+
+        return [kanji[0], kanaData[highestSimilarityIndex].word_id]
+      }
+      
 
       emptyResults.push(kanji as never)
-
-      return [kanji[0], data]
     }),
   )
 
@@ -92,14 +161,11 @@ async function processKanjiArray() {
 
   await Bun.write(
     'kanjiArrayWithSearchResults.json',
-    JSON.stringify(kanjiArrayWithSearchResults),
+    JSON.stringify(kanjiArrayWithSearchResults.filter((x) => x != null)),
   )
-
-  // Save the emptyResults array to a file
-
-  await Bun.write('emptyResults.json', JSON.stringify(emptyResults))
 }
 
 await processKanjiArray()
 
-Bun.write('kanji.json', JSON.stringify(responsesToWrite))
+console.log(emptyResults.length)
+await Bun.write('emptyResults.json', JSON.stringify(emptyResults))
