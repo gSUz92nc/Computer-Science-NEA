9f2f8893a516b03f375d93efd839bd0f9a932eae
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-12-16 14:03:25 +0000

    Testing the tip implementation since school wifi blocks api request

--- src/routes/practice/+page.svelte
+++ src/routes/practice/+page.svelte
@@ -5,21 +5,42 @@
   const { input, handleSubmit, messages, setMessages } = useChat()
 
   const scenario = 'You are a Highschool teacher and the user is a student'
-  
+
+  let selectedMessage: string | null = null;
   let tip: HTMLDialogElement
 
   onMount(() => {
-    
     tip = document.getElementById('tipModal') as HTMLDialogElement
-    
+
     setMessages([
       {
         role: 'system',
         content: `You are a Japanese tutor who helps students learn the language by roleplaying different scenarios. Right now you are going to act out this scenario: ${scenario}. Respond in natural Japanese, using Kanji/Hiragana/Katakana. Your message response format should look like this: \n\n(First address anything incorrect that occured in the user's last message in English. make sure you correct anything that is wrong or give them guidance on how to better improve their sentences and make them sound more natural)\n\n(Everything in Kanji when possible or when it makes sense)\n\n(Rewrite with no kanji, replace all the kanji with the appropriate hiragana)\n\nTranslation in English`,
-        id: '', // This is here for ts, not sure why it doesn't like not including it
+        id: '',
       },
     ])
   })
+
+  const showTranslation = async (content: string) => {
+    selectedMessage = content;
+    const response = await fetch('/api/explain', {
+      method: 'POST',
+      body: JSON.stringify({ message: content }),
+    });
+    const explanation = await response.json();
+    tip.querySelector('.modal-content').innerHTML = `
+      <h3 class="text-lg font-bold">Translation & Grammar</h3>
+      <div class="py-4">
+        <p class="font-bold">Original:</p>
+        <p>${content}</p>
+        <p class="font-bold mt-4">Translation:</p>
+        <p>${explanation.translation}</p>
+        <p class="font-bold mt-4">Grammar Notes:</p>
+        <p>${explanation.grammar}</p>
+      </div>
+    `;
+    tip.showModal();
+  }
 </script>
 
 <div class="flex flex-col h-screen">
@@ -28,11 +49,12 @@
   </div>
   <dialog id="tipModal" class="modal">
     <div class="modal-box">
-      <h3 class="text-lg font-bold">Hello!</h3>
-      <p class="py-4">Press ESC key or click the button below to close</p>
+      <div class="modal-content">
+        <h3 class="text-lg font-bold">Hello!</h3>
+        <p class="py-4">Press ESC key or click the button below to close</p>
+      </div>
       <div class="modal-action">
         <form method="dialog">
-          <!-- if there is a button in form, it will close the modal -->
           <button class="btn">Close</button>
         </form>
       </div>
@@ -44,28 +66,28 @@
         <div
           class={`chat ${message.role === 'user' ? 'chat-end' : 'chat-start'}`}
         >
-          <div class="chat-bubble whitespace-pre-line">
+          <div class="chat-bubble whitespace-pre-line cursor-pointer" on:click={() => showTranslation(message.content)}>
             {message.content}
             {#if message.role === 'assistant'}
-                <button on:click={() => tip.showModal()}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-box-arrow-up-right"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
-                />
-                <path
-                  fill-rule="evenodd"
-                  d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
-                />
-              </svg>
-                </button>
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-box-arrow-up-right"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
+                  />
+                </svg>
+              </button>
             {/if}
           </div>
         </div>
